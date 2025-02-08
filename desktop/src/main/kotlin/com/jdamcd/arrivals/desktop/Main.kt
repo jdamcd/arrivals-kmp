7894de4d7965fbdac59ebd4f3a05114f6c5e484a
Jamie McDonald <jdamcd@gmail.com>
2025-02-08 22:30:15 +0000

    Format

--- desktop/src/main/kotlin/com/jdamcd/arrivals/desktop/Main.kt
+++ desktop/src/main/kotlin/com/jdamcd/arrivals/desktop/Main.kt
@@ -2,14 +2,12 @@ package com.jdamcd.arrivals.desktop
 
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPlacement
-import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import com.jdamcd.arrivals.Arrivals
